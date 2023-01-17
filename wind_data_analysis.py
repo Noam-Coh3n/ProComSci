306e5235bd7addef0aecdc59d5588fca15c2b7d5
Erik Leonards <erikleonards1@gmail.com>
2023-01-17 15:50:33 +0100

    added remove_data_above_height that removes all rows of file with height over certain value

--- wind_data_analysis.py
+++ wind_data_analysis.py
@@ -2,22 +2,42 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+import re
 
 from convert_data import convert_data
-
 
 def inv_prop(x, a, b, c):
     return a / (x + b) + c
 
-# def fit_data(x, y):
-#     X = np.array([[1 / x_i, 1] for x_i in x]).transpose()
-#     return np.linalg.inv((X @ X.transpose())) @ X @ y
+def remove_data_above_height(height):
+    data_file = open('vegas_wind_data.txt', 'r')
+    new_data_file = open('vegas_compressed.txt', 'w')
+    for line in data_file:
+        line = re.sub('[A-Z]', ' ', line)
+        height = re.search('^[\-0-9]+ *[\-0-9]* *[\-0-9]* *([\-0-9]*)', line)
+        if line[0] == '#' \
+           or (line[0] != '3' and height and int(height.group(1)) < 4000):
+            new_data_file.write(line)
 
+def retrieve_rho_and_wind():
+    data_file = open('vegas_wind_data.txt', 'r')
+    data = str(data_file.read())[:10000]
 
-if __name__ == '__main__':
+    new_data = pd.DataFrame(columns=['year', 'month', 'day', 'rho', 'w_x', 'w_y'])
+
+    # Remove chars
+    data = re.sub('[a-zA-Z]', '', data)
+    # data = re.sub(' +', ' ', data)
+
+    dates = re.findall('#.*?([0-9]{4}) ([0-9]{2}) ([0-9]{2}) .*?\n([\s\S]*?)\n30   100', data)
+    # date1 = re.sub(' +', ' ', dates[0])
+    print(dates[0])
+    # for date in dates:
+    #     print(date)
+
+def plot_test_data():
     data = np.array(pd.read_csv('test_data.txt', sep=' '))
     data[:, [0, 1]] = data[:, [1, 0]]
-    print(data.transpose())
     height, rho, w_x, w_y = convert_data(data)
     params, _ = curve_fit(inv_prop, height, rho)
 
@@ -30,5 +50,9 @@ if __name__ == '__main__':
     plt.plot(height, w_y, 'b', label=r'$w_y$')
     plt.show()
 
+if __name__ == '__main__':
+
+    remove_data_above_height(5000)
+    # plot_test_data()
 
 
