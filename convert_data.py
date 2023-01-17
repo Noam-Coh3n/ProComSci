24d678c46cd29975fa095bcd43c58915b02ddce4
Erik Leonards <erikleonards1@gmail.com>
2023-01-17 11:26:39 +0100

    bugs fixed

--- convert_data.py
+++ convert_data.py
@@ -6,8 +6,9 @@ def density(pressure, temperature):
     return (pressure * m_air) / (k_B_air * temperature)
 
 def convert_data(data):
-    rho = density(data['pressure'], data['temp'] / 10 + 273)
-    radials = data['wind_dir'] * 2 * np.pi / 360 # Convert deg to radials
-    w_x = np.sin(radials) * data['wind_vel']
-    w_y = np.cos(radials) * data['wind_vel']
-    return np.vstack((data['height'] / 10, rho, w_x, w_y))
+    height, pressure, temp, wind_dir, wind_vel = data.transpose()
+    rho = density(pressure, temp / 10 + 273)
+    radials = wind_dir * 2 * np.pi / 360 # Convert deg to radials
+    w_x = np.sin(radials) * wind_vel / 10
+    w_y = np.cos(radials) * wind_vel / 10
+    return np.vstack((height / 10, rho, w_x, w_y))
