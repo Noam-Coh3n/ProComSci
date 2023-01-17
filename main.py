ce91c08d702047dd181d4ce25a83118704267bbb
Jelle Sipkes <sipkes.point@outlook.com>
2023-01-17 17:20:36 +0100

    Created plots for location, speed and velocity per time interval

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import numpy as np
 from diver import Diver
 import matplotlib.pyplot as plt
 
+STEP_SIZE = 0.005
+
 # Setup variables
 wind_list = np.array([[-30, 0], [10, 0], [30, 60]])
 air_pres_list = [100000]
@@ -10,16 +12,36 @@ temp_list = [280]
 # Add a diver.
 myDiver = Diver(x=np.array([0.,0.,3600.]), vel=np.array([0.,-600.,0.]), 
             wind=wind_list, air_pressure=air_pres_list, temperature=temp_list,
-            h_shute=200)
+            h_shute=200, stepsize=STEP_SIZE)
 # Run model.
 while True:
     if myDiver.x[2] <= 0: 
         break
     myDiver.move()
 
-# Plot.
-plt.figure(300)
-plt.plot([x[2] for x in myDiver.x_list])
+# Plot location.
+for i, direction in enumerate(['x', 'y', 'z']):
+    plt.figure(f"{direction}-direction, location")
+    time = [i * STEP_SIZE for i in range(len(myDiver.x_list))]
+
+    # Location.
+    plt.plot(time, [x[i] for x in myDiver.x_list], label="location")
+
+    plt.legend()
+    plt.xlabel("time (sec)")
+
+# Plot velocity and acceleration.
+for i, direction in enumerate(['x', 'y', 'z']):
+    plt.figure(f"{direction}-direction, velocity and acceleration")
+    time = [i * STEP_SIZE for i in range(len(myDiver.x_list))]
+
+    # Location.
+    plt.plot(time, [v[i] for v in myDiver.v_list], label="velocity")
+    plt.plot(time, [a[i] for a in myDiver.a_list], label="acceleration")
+
+    plt.legend()
+    plt.xlabel("time (sec)")
+
 plt.show()
 
 
@@ -29,3 +51,6 @@ plt.show()
 # myVisual.add_diver(myDiver)
 # myVisual.select_line_interval(300)
 # myVisual.run(slowed=10)
+
+
+# Pyplot en Pygame kunnen niet tegenlijkertijd, dan krijg je een error.
