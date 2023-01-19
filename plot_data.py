6ad1567be85320e2d2d29f518001eaed9b9a5b64
Erik Leonards <erikleonards1@gmail.com>
2023-01-19 22:00:48 +0100

    Added change of wind speed per meter

---
+++ plot_data.py
@@ -0,0 +1,125 @@
+#
+# Name: Lars Bijvoet, Erik Leonards, Noam Cohen & Jelle Sipkes
+# Study: Double Bachelor mathematics and computer science.
+#
+# plot_data.py:
+# This file plots the dataset, determines the average value
+# and standard deviation.
+
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+from IPython.display import Latex
+import numpy as np
+
+# Define colors for the plots.
+COLOR_AVG = '#319600'
+COLOR_STD_DEV = '#00696b'
+COLOR_FITTED_AVG = '#1c0f00'
+COLOR_FITTED_STD_DEV = '#700000'
+COLOR_DATASET = '#02006b'
+
+
+def fit_func(x: float, a: float, b: float, c: float) -> float:
+    """
+    Quadratic function.
+    """
+    return a * x ** 2 + b * x + c
+
+
+def plot_data(data, data_variable=['windspeed', 'rho']):
+    """
+    Fit and plot the data.
+    """
+
+    ylabels = [r'$v (m/s)$', r'$v (m/s)$', r'$\rho (kg / m^3)$']
+    title_list = []
+    if 'rho' in data_variable:
+        title_list.append(r'The air density $\rho$')
+    if 'wind' in data_variable:
+        title_list.extend([r'Wind speed in the $x$ direction',
+                           r'Wind speed in the $y$ direction'])
+    if 'change of speed' in data_variable:
+        title_list.extend([r'Change of wind speed in $x$ direction',
+                           r'Change of wind speed in $y$ direction'])
+
+    plt.figure(figsize=(14, 8), dpi=100)
+    nr_of_subplots = len(data) - 1
+
+    for i, title in enumerate(title_list, 1):
+        plt.subplot(int(f'1{nr_of_subplots}{i}'))
+        plot_and_fit(data[0], data[i], xlabel='height', ylabel=ylabels[i],
+                     title=title)
+
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_avg_and_std_dev(reg_x_vals, bins):
+    # Calculate the average and standard deviation.
+    avg = [sum(y for (_, y) in bin) / len(bin) for bin in bins]
+    std_dev = [np.sqrt(sum((y - avg[i])**2 for (_, y) in bin) / (len(bin) - 1))
+               for i, bin in enumerate(bins)]
+
+    # Fit the average and standard deviation using a quadratic polynomial.
+    params_avg, _ = curve_fit(fit_func, reg_x_vals, avg)
+    params_std_dev, _ = curve_fit(fit_func, reg_x_vals, std_dev)
+
+    fitted_y_vals = fit_func(reg_x_vals, *params_avg)
+    fitted_std_dev_vals = fit_func(reg_x_vals, *params_std_dev)
+
+    # Plot the standard deviation and average.
+    plt.fill_between(reg_x_vals,
+                     np.array(avg) - std_dev,
+                     np.array(avg) + std_dev,
+                     alpha=0.7, color=COLOR_STD_DEV, label='std dev')
+    plt.plot(reg_x_vals, avg, color=COLOR_AVG, label='avg values')
+
+    a1, b1, c1 = params_avg
+    a2, b2, c2 = params_std_dev
+
+    print(f'Average fitted:  {a1:.5e}x^2+{b1:.5e}x+{c1:.5e}')
+    print(f'Std dev fitted:  {a2:.5e}x^2+{b2:.5e}x+{c2:.5e}')
+    print()
+
+    # Plot the quadratic polynomials corresponding to the avg and std dev.
+    plt.plot(reg_x_vals, fitted_y_vals, color=COLOR_FITTED_AVG,
+             label='avg value fit: quadratic')
+    plt.plot(reg_x_vals, fitted_y_vals + fitted_std_dev_vals,
+             color=COLOR_FITTED_STD_DEV, label='std dev fit: quadratic')
+    plt.plot(reg_x_vals, fitted_y_vals - fitted_std_dev_vals,
+             color=COLOR_FITTED_STD_DEV)
+
+
+def plot_and_fit(x_vals: list, y_vals: list, xlabel: str = '',
+                 ylabel: str = '', title: str = '') -> None:
+    """
+    This function does the following:
+    - Plot the given data as a scatter plot.
+    - Plot the average value and standard deviation at every point.
+    - Plot the average value fitted to a quadratic polynomial.
+    - Plot the standard deviation fitted to a quadratic polynomial.
+    """
+    stepsize = 50
+    reg_x_vals = np.arange(min(x_vals), max(x_vals), stepsize)
+    # Order the data in bins.
+    nr_of_bins = int(np.ceil(max(x_vals) / stepsize))
+    bins = [[] for _ in range(nr_of_bins)]
+    for x, y in zip(x_vals, y_vals):
+        index = int(np.floor(x / stepsize))
+        if index == nr_of_bins:
+            index -= 1
+        bins[index].append((x, y))
+
+    # Plot the dataset.
+    plt.scatter(x_vals, y_vals, alpha=0.4, s=1,
+                c=COLOR_DATASET, label='observed data')
+
+    plot_avg_and_std_dev(reg_x_vals, bins)
+
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.legend()
+
+
+
--- wind_data_analysis.py
+++ wind_data_analysis.py
@@ -1,6 +1,6 @@
 #
 # Name: Lars Bijvoet, Erik Leonards, Noam Cohen & Jelle Sipkes
-# Study: Dubbel Bachelor mathematics and Informatica.
+# Study: Double Bachelor mathematics and computer science.
 #
 # wind_data_analysis.py:
 # This file does the following:
@@ -15,36 +15,23 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
-import numpy as np
 from os import path
 import re
 
 from constant import m_air, k_B_air
-
-# Define colors for the plots.
-COLOR_AVG = '#319600'
-COLOR_STD_DEV = '#00696b'
-COLOR_FITTED_AVG = '#1c0f00'
-COLOR_FITTED_STD_DEV = '#700000'
-COLOR_DATASET = '#02006b'
+from plot_data import plot_data
 
 # Numbers in the dataset that indicate a corrupt entry.
 CORRUPT_VALUES = {-8888, -9999}
 
-def density(pressure : float, temperature : float) -> float:
+def density(pressure: float, temperature: float) -> float:
     """
     Calculates the air density from the air pressure and temperature.
     """
     return (pressure * m_air) / (k_B_air * temperature)
 
-def fit_func(x, a, b, c):
-    """
-    Quadratic function.
-    """
-    return a * x ** 2 + b * x + c
 
-def clean_up_data_line(line):
+def clean_up_data_line(line: str) -> tuple:
     """
     Given a line of data from a dataset, this function
     returns the height and a string containing the height, pressure,
@@ -57,11 +44,13 @@ def clean_up_data_line(line):
 
     # Split the line and return the data if none of the values are corrupt.
     _, _, pressure, height, temp, _, _, dir, vel = line.split(' ')
-    if CORRUPT_VALUES.isdisjoint({int(pressure), int(height), int(temp), int(dir), int(vel)}):
+    if CORRUPT_VALUES.isdisjoint({int(pressure), int(height),
+                                  int(temp), int(dir), int(vel)}):
         return int(height), ' '.join([height, pressure, temp, dir, vel])
     return None, None
 
-def remove_data_above_height(max_height):
+
+def remove_data_above_height(max_height: int) -> None:
     """
     Removes all lines of data from the Las Vegas wind data set that
     are measured at a height above the max_height.
@@ -88,7 +77,33 @@ def remove_data_above_height(max_height):
         if height and line[0] != '3' and height < max_height:
             new_data_file.write(line_data)
 
-def retrieve_data_from_dates(dates):
+
+def retrieve_rho_and_wind(data: list) -> list:
+    """
+    Returns the height, air density (rho) and wind data when being given
+    the height, pressure, temperature and wind data.
+    """
+    result = []
+    for data_item in data:
+        # Converts the data string into a numpy array.
+        data_item = np.fromstring(data_item, sep=' ')
+        data_item = np.reshape(data_item, (-1, 5))
+
+        # Calculate the needed values from the given data.
+        height, press, temp, dir, vel = data_item.transpose()
+        height -= 697  # Natural elevation of Las Vegas.
+        rho = density(press, temp / 10 + 273)
+        radials = dir * 2 * np.pi / 360  # Convert deg to radials.
+
+        # Wind.
+        w_x = np.sin(radials) * vel / 10
+        w_y = np.cos(radials) * vel / 10
+        result.append(np.vstack((height, rho, w_x, w_y)))
+
+    return result
+
+
+def retrieve_data_from_dates(dates: list) -> list:
     """
     Returns the air density (rho) and wind data of the specified dates.
     """
@@ -97,7 +112,7 @@ def retrieve_data_from_dates(dates):
 
     # Extends the result_data with the data of all the specified dates.
     for (y, m, d) in dates:
-        data = re.findall(f'# {y} {m:02} {d:02} #\n([\s\S]*?)#', raw_data)
+        data = re.findall(rf'# {y} {m:02} {d:02} #\n([\s\S]*?)#', raw_data)
         result_data.extend(retrieve_rho_and_wind(data))
 
     # Combines all the data to get 4 lists: height, rho, w_x, w_y.
@@ -107,107 +122,43 @@ def retrieve_data_from_dates(dates):
             data[i].extend(item[i])
     return data
 
-def retrieve_rho_and_wind(data):
-    result = []
-    for data_item in data:
-        data_item = np.fromstring(data_item, sep=' ')
-        data_item = np.reshape(data_item, (-1, 5))
 
-        height, press, temp, dir, vel = data_item.transpose()
-        height -= 697 # Natural elevation of Las Vegas.
-        rho = density(press, temp / 10 + 273)
-        radials = dir * 2 * np.pi / 360 # Convert deg to radials
-        w_x = np.sin(radials) * vel / 10
-        w_y = np.cos(radials) * vel / 10
-        result.append(np.vstack((height, rho, w_x, w_y)))
+def all_dates() -> list:
+    """
+    Return list of all dates from 2018 to 2023.
+    """
+    return [(y, m, d) for y in range(2018, 2023)
+            for m in range(1, 13) for d in range(1, 32)]
 
-    return result
 
-def plot_data_from_date(*date):
-    height, rho, w_x, w_y = retrieve_data_from_dates([date])
-    plt.figure(figsize=(10, 8), dpi=100)
-    plt.subplot(121)
-    plot_rho(height, rho)
-    plt.subplot(122)
-    plot_wind(height, w_x, w_y)
-    plt.show()
+def change_of_wind(dates):
+    height, _, w_x, w_y = retrieve_data_from_dates(dates)
 
-def all_dates():
-    return [(y, m, d) for y in range(2018, 2023) \
-                      for m in range(1, 13) \
-                      for d in range(1, 32)]
+    rate_changes_x = []
+    rate_changes_y = []
+    new_height = []
+    for cur_height, cur_w_x, cur_w_y in zip(height, w_x, w_y):
+        if cur_height == 0:
+            prev_height = cur_height
+            prev_w_x = cur_w_x
+            prev_w_y = cur_w_y
+            continue
+        h_difference = cur_height - prev_height
 
-def plot_and_fit(x_vals, y_vals, xlabel='', ylabel='', title=''):
-    stepsize = 50
-    reg_x_vals = np.arange(min(x_vals), max(x_vals), stepsize)
+        rate_changes_x.append((cur_w_x - prev_w_x) / h_difference)
+        rate_changes_y.append((cur_w_y - prev_w_y) / h_difference)
 
-    nr_of_bins = int(np.ceil(max(x_vals) / stepsize))
-    bins = [[] for _ in range(nr_of_bins)]
-    for x, y in zip(x_vals, y_vals):
-        index = int(np.floor(x / stepsize))
-        bins[index].append((x, y))
+        avg_height = int(np.floor((prev_height + cur_height) / 2))
+        new_height.append(avg_height)
 
-    # Plot the dataset.
-    plt.scatter(x_vals, y_vals, alpha=0.4, s=1, c=COLOR_DATASET, label='observed data')
+        prev_height = cur_height
+        prev_w_x = cur_w_x
+        prev_w_y = cur_w_y
 
-    # Calculate the average and standard deviation.
-    avg = [sum(y for (_, y) in bin) / len(bin) for bin in bins]
-    std_dev = [np.sqrt(sum((y - avg[i])**2 for (_, y) in bin) / (len(bin) - 1)) for i, bin in enumerate(bins)]
+    return new_height, rate_changes_x, rate_changes_y
 
-    # Fit the average and standard deviation using a quadratic polynomial.
-    params_avg, _ = curve_fit(fit_func, reg_x_vals, avg)
-    params_std_dev, _ = curve_fit(fit_func, reg_x_vals, std_dev)
-
-    fitted_y_vals = fit_func(reg_x_vals, *params_avg)
-    fitted_std_dev_vals = fit_func(reg_x_vals, *params_std_dev)
-
-    # Plot the standard deviation and average.
-    plt.fill_between(reg_x_vals,
-                     np.array(avg) - std_dev,
-                     np.array(avg) + std_dev,
-                     alpha=0.7, color=COLOR_STD_DEV, label='std dev')
-    plt.plot(reg_x_vals, avg, color=COLOR_AVG, label='avg values')
-
-    # Plot the quadratic polynomials fitted to the average and standard deviation.
-    plt.plot(reg_x_vals, fitted_y_vals, color=COLOR_FITTED_AVG,
-             label='avg value fit: quadratic')
-    plt.plot(reg_x_vals, fitted_y_vals + fitted_std_dev_vals,
-             color=COLOR_FITTED_STD_DEV, label='std dev fit: quadratic')
-    plt.plot(reg_x_vals, fitted_y_vals - fitted_std_dev_vals,
-             color=COLOR_FITTED_STD_DEV)
-
-    plt.title(title)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.legend()
-
-def plot_rho(height, rho):
-    plot_and_fit(height, rho, xlabel='height', ylabel=r'$\rho (kg / m^3)$',
-                 title=r'The air density $\rho$')
-
-def plot_wind(height, vel, dir):
-    plot_and_fit(height, vel, xlabel='height', ylabel=r'$v (m/s)$',
-                 title=rf'Wind speed in the ${dir}$ direction')
-
-def average_data(plot=False):
-    data = retrieve_data_from_dates(all_dates())
-
-    if plot:
-        plt.figure(figsize=(14, 8), dpi=100)
-        plt.subplot(131)
-        plot_rho(data[0], data[1])
-        plt.subplot(132)
-        plot_wind(data[0], data[2], 'x')
-        plt.subplot(133)
-        plot_wind(data[0], data[3], 'y')
-        plt.tight_layout()
-        plt.show()
-    return data
 
 if __name__ == '__main__':
     # remove_data_above_height(5500)
-    # plot_data_from_date(2018, 1, 1)
-    # print(retrieve_rho_and_wind(2018, 1, 1))
-    average_data(plot=True)
-
-
+    data = change_of_wind(all_dates())
+    plot_data(data, data_variable='change of speed')
