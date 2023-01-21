7d80a8077c29cf1ec4115fb53cba1a7d8e28ae09
Erik Leonards <erikleonards1@gmail.com>
2023-01-21 17:47:33 +0100

    Applied parallel computing to increase computing power.

--- integration_error.py
+++ integration_error.py
@@ -1,12 +1,36 @@
-import integration
 from diver import Diver
 import numpy as np
 import matplotlib.pyplot as plt
+import multiprocessing
 
 # Define parameters used for the diver and define the methods that will be
 # comperd.
-h = 0.001
-method = ['RK4', 'Euler', 'Central diff', 'Pred-corr']
+h = 0.0001
+methods = ['RK4', 'Euler', 'Central diff', 'Pred-corr']
+
+
+def simulate_method(params):
+    method, h_vals, y_part = params
+    sum_error = []
+    for h in h_vals:
+        # Add a diver.
+        myDiver = Diver(x=np.array([0., 0., 3600.]),
+                        vel=np.array([45., 0., 0.]),
+                        h_opening=200, stepsize=h, seed=0)
+        # Run model with the different models and stepsizes.
+        myDiver.simulate_trajectory(method)
+
+        # Store the data.
+        y_n = [x[0] for x in myDiver.x_list]
+        step = len(myDiver.x_list) / 1000
+
+        # Determine the error and take the sum
+        y_n_part = [y_n[int(np.floor(l))]
+                    for l in np.arange(0, len(y_n), step)
+                    if int(np.floor(l)) != len(y_n)]
+        sum_error.append(sum(np.abs(np.array(y_part) -
+                                    np.array(y_n_part))))
+    return sum_error
 
 
 def simulate_error(h_vals):
@@ -17,7 +41,7 @@ def simulate_error(h_vals):
     """
     # Get diver data with stepsize 0.001.
     myDiver = Diver(x=np.array([0., 0., 3600.]),
-                    vel=np.array([0., -600., 0.]),
+                    vel=np.array([45., 0., 0.]),
                     h_opening=200, stepsize=h, seed=0)
 
     # Simulate the diver with Runge-kutta order 4
@@ -29,32 +53,18 @@ def simulate_error(h_vals):
     y_part = [y[int(np.floor(i))] for i in np.arange(0, len(y), step_size)
               if int(np.floor(i)) != len(y)]
 
-    # Loop through all the methods and loop through all the stepsize values.
-    for i in method:
-        sum_error = []
-        for j in h_vals:
-            # Add a diver.
-            myDiver = Diver(x=np.array([0., 0., 3600.]),
-                            vel=np.array([0., -600., 0.]),
-                            h_opening=200, stepsize=j, seed=0)
-            # Run model with the different models and stepsizes.
-            myDiver.simulate_trajectory(i)
+    pool = multiprocessing.Pool()
+    sum_errors = pool.map(simulate_method, [(method, h_vals, y_part) for method in methods])
+    pool.close()
 
-            # Store the data.
-            y_n = [x[0] for x in myDiver.x_list]
-            step = len(myDiver.x_list) / 1000
-
-            # Determine the error and take the sum
-            y_n_part = [y_n[int(np.floor(l))]
-                        for l in np.arange(0, len(y_n), step)
-                        if int(np.floor(l)) != len(y_n)]
-            sum_error.append(sum(np.abs(np.array(y_part) -
-                                 np.array(y_n_part))))
-        # Plot the sum of the error.
-        plt.figure("Error methods.")
-        plt.plot(h_vals, sum_error, label=i)
-        plt.legend()
-        plt.xlabel("Step size")
-        plt.ylabel("Sum of error")
-        plt.yscale("log")
+    # Plot the sum of the error.
+    plt.figure("Error methods.")
+    for method, sum_error in zip(methods, sum_errors):
+        plt.plot(h_vals, sum_error,label=method)
+    plt.legend()
+    plt.yscale("log")
+    plt.xscale("log")
+    plt.xlabel("Step size")
+    plt.ylabel("Sum of error")
     plt.show()
+
