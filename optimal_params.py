bd00fd22c518600dd2b37b26c0ae7e2cd4d33dc0
Erik Leonards <erikleonards1@gmail.com>
2023-01-23 11:19:12 +0100

    added to find optimal parameters for the given scenario

---
+++ optimal_params.py
@@ -0,0 +1,15 @@
+from constants import *
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def find_optimal_params(dir_vals):
+    for dir in dir_vals:
+        for
+
+if __name__ == '__main__':
+    dir_vals = np.linspace(0, 2 * np.pi, 3)
+    x_vals = np.linspace(0, 2000, 3)
+    y_vals = np.linspace(0, 500, 3)
+    
+    find_optimal_params(dir_vals)
