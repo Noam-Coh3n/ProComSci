447b22a9cfb1cde3f7ee3fe37a1ce4088a78de31
Larsokillerz <lars.bijvoet@student.uva.nl>
2023-01-24 12:18:00 +0100

    updated optimal_params.py

--- optimal_params.py
+++ optimal_params.py
@@ -1,4 +1,4 @@
-from constants import *
+import constants as const
 import matplotlib.pyplot as plt
 import numpy as np
 import multiprocessing
@@ -14,16 +14,16 @@ NUMBER_D = 1
 
 def simulate_params(params):
     x, y, d = params[:3]
-    pos = np.array([x, y, h_airplane])
-    v = np.array([np.cos(d) * v_airplane, np.sin(d) * v_airplane, 0])
+    pos = np.array([x, y, const.h_airplane])
+    v = np.array([np.cos(d) * const.v_airplane, np.sin(d) * const.v_airplane, 0])
     wind = Wind_generator((0, np.inf), (0, np.inf))
 
     nr_of_successes = 0
     for seed in params[3:]:
         myDiver = Diver(x=pos, velocity=v, wind=wind, stepsize=H_VAL, seed=seed)
-        myDiver.simulate_trajectory('Euler')
+        myDiver.simulate_trajectory('rk4')
         x, y, _ = myDiver.x
-        if x ** 2 + y ** 2 < radius_landing_area ** 2:
+        if x ** 2 + y ** 2 < const.radius_landing_area ** 2:
             nr_of_successes += 1
 
     return params[:3] + [nr_of_successes / NR_OF_SIMS]
