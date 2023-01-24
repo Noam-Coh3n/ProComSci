de008bdb9ddb98d43c3f39d7cf54ae9860e0d636
Noam Cohen <noam@cohen.nl>
2023-01-24 11:59:52 +0100

    Removing visualisation functionality from Diver class

--- diver.py
+++ diver.py
@@ -1,37 +1,36 @@
 import numpy as np
 import integration
-from constants import *
-from wind_and_rho_generator import *
+from wind_and_rho_generator import rho
+import constants as const
 
 
 class Diver():
 
-    def __init__(self, x: np.array, vel: np.array, wind,
+    def __init__(self, x: np.array, velocity: np.array, wind,
                  stepsize: float = 0.001, seed=None):
 
-        self.F_gravity = np.array([0, 0, -m_diver * g])
+        self.F_gravity = np.array([0, 0, -const.m_diver * const.g])
         self.rho = rho
         self.wind_x = wind.wind(wind_dir='x', seed=seed)
         self.wind_y = wind.wind(wind_dir='y', seed=seed)
 
         self.x = x
-        self.v = vel
+        self.v = velocity
         self.a = [0, 0, 0]
         self.x_list = []
         self.v_list = []
         self.a_list = []
 
         self.x_z_0 = self.x[2]
-        self.h_opening = h_opening
-        self.chute_pos = 0
+        # self.chute_pos = 0
 
-        self.x2pos()
-        self.pos_list = []
+        # self.x2pos()
+        # self.pos_list = []
 
         self.step_size = stepsize
 
     def _add_new_pos(self):
-        self.pos_list.append(np.copy(self.pos))
+        # self.pos_list.append(np.copy(self.pos))
         self.x_list.append(np.copy(self.x))
         self.v_list.append(np.copy(self.v))
         self.a_list.append(np.copy(self.a))
@@ -42,17 +41,16 @@ class Diver():
         _, _, x_z, v_x, v_y, v_z = data
 
         # Free fall
-        if x_z > self.h_opening:
-            C = C_diver
-            A = A_diver
+        if x_z > const.h_opening:
+            C = const.C_diver
+            A = const.A_diver
 
         # Under canopy
         else:
-            if self.chute_pos == 0:
-                self.chute_pos = self.pos[2]
-
-            C = C_chute
-            A = A_chute
+            # if self.chute_pos == 0:
+            #     self.chute_pos = self.pos[2]
+            C = const.C_chute
+            A = const.A_chute
 
         rho = self.rho(x_z)
         w_x = self.wind_x(x_z)
@@ -64,7 +62,7 @@ class Diver():
 
         F = F_drag + self.F_gravity
 
-        [a_x, a_y, a_z] = F / m_diver
+        [a_x, a_y, a_z] = F / const.m_diver
         return np.array([v_x, v_y, v_z, a_x, a_y, a_z])
 
     # Integration methods.
@@ -83,13 +81,13 @@ class Diver():
         self.a = k[3:]
 
     def move(self, method):
-        self.x2pos()
+        # self.x2pos()
         self._add_new_pos()
         self.integration(method)
 
-    def x2pos(self):
-        """Get position in cube from x location in real world."""
-        self.pos = self.x * np.array([2, 2, -1]) + np.array([0, 0, self.x_z_0/2])
+    # def x2pos(self):
+    #     """Get position in cube from x location in real world."""
+    #     self.pos = self.x * np.array([2, 2, -1]) + np.array([0, 0, self.x_z_0/2])
 
     def simulate_trajectory(self, method):
         while True:
