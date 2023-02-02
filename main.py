460922c1c7fe7c466136a15102599d30fbb8f063
Erik Leonards <erikleonards1@gmail.com>
2023-02-02 19:50:04 +0100

    changed seed of diver to 0

--- main.py
+++ main.py
@@ -129,7 +129,7 @@ if __name__ == '__main__':
         # Add a diver.
         x = np.array([0., 0., const.h_plane])
         velocity = np.array([const.v_plane, 0., 0.])
-        myDiver = Diver(x, velocity, wind, STEP_SIZE)
+        myDiver = Diver(x, velocity, wind, STEP_SIZE, seed=0)
 
         # Run model.
         myDiver.simulate_trajectory()
